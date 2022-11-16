ecb4d05259ffe450e73695490fc7d18bec063516
Saurav Dudhate <75058098+saurav2127@users.noreply.github.com>
2022-11-16 23:56:45 +0530

    Add files via upload

--- data_entry.sql
+++ data_entry.sql
@@ -1,76 +1,164 @@
-INSERT INTO Users (UID, username, password, role_id)
+-- addresses
+INSERT INTO addresses(address_id, address_line1, address_line2, city, state, country, zipcode)
+    VALUES 
+	 (1,'203', 'Morni Appartments', 'Chandigarh','Punjab','India','239122'),
+	 (2,'1099', 'Sunset Appartments', 'Nanded','Maharashtra','India','457329'),
+	 (3,'123', 'Sunrise Appartments', 'Bhilai','Chattisgarh','India','135289'),
+	 (4,'451', 'Galaxy Appartments', 'Pune','Maharashtra','India','675281'),
+	 (5,'666', 'Suncity Appartments', 'Lucknow','UP','India','657329'),
+	 (6,20400, 'Phoenix', NULL,'AZ','USA','AZ85027'),
+	 (11,45, 'Vanier Park', 'Kitchener','ON','Canada','Sd3 d35'),
+	 (12,41, 'Greenfield', 'London','ON','Canada','234 987'),
+	 (13,89, 'Jacob Rd', 'Paris','ON','Canada','467 289'),
+	 (14,85, 'Martin Street', 'Ottawa','BC','Canada','263 987'),
+	 (15,78, 'Josseph St. Street', 'Guelph','BC','Canada','267 387'),
+	 (16,156, 'James Road', NULL,'AZ','USA','263 762'),
+	 (17,7598, 'Atomic Street', 'Ottawa','New York','USA','756 145'),
+	 (18,5476, 'Saint Jake Rd', 'NULL','San Jose','USA','675 846'),
+	 (19,7465, 'Thames Rd', 'NUll','Gujarat','India','145 895'),
+	 (101,'A Quarters', 'IIT Patna', 'Bihta','Bihar','India','801103'),
+	 (102,'B Quarters', 'IIT Patna', 'Bihta','Bihar','India','801103'),
+	 (103,'C Quarters', 'IIT Patna', 'Bihta','Bihar','India','801103'),
+	 (104,'D Quarters', 'IIT Patna', 'Bihta','Bihar','India','801103');
+	 
+
+-- pre filled data of room type, employee, guesthouses, department, guestHouse_services, 
+
+INSERT INTO department(department_name, department_description)
     VALUES
-     (2, 'admin', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 1),
-     (3, 'aaf', 'aafaffaffafaff', 2),
-     (6, 'gh1', '$2b$10$2MpPC40eJJQhxbZmjbJR.eqpPdsxgVn75L92.gfA.OG.BLB9VpLUq', 2),
-     (7, 'admin2', '$2b$10$4SUFSO6shIPkH349yReyxut9euCL.2hCb/QcLLOIDSCU2ZeRivgsi', 1),
-     (8, 'admin33', '$2b$10$itnEizweyE5rWvMjYfB/E.nxGYsj9sKmgAtC9eJYQw/aSMh.W3q0m', 4),
-     (9, 'Jack', 'afafasfasf', 2),
-     (11, 'Regular', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 201),
-     (12, 'CC1', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 202),
-     (14, 'Help1', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 203),
-     (15, 'Help2', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 203),
-     (16, 'CC2', '$2b$10$Ys9CxSNzb2QS8FRq..Of3eG2kowa4DNGRDX3fuufzZuEnIRfJblxu', 202),
-     (17, 'RegularCook', '$2b$10$2OHgcWW4GqprK5ovD7Y7ZucfGJma.z35R3tFiuqpygntH/3g0S0W2', 201),
-     (18, 'CookSupervisor', '$2b$10$fKcaVruqtdprMLbwo.Ad8.3jiLRGymJUYqW8fryEifdRgSS628Bje', 204),
-     (19, 'Staff', '$2b$10$m.cIzj8sefC1X/ZOgZhkU.dF0QzOYxwM1UI17/XvLYcmpIUGBJYji', 205),
-     (20, 'GeneralUser', '$2b$10$6a5NzeDgLFSSsLbD9X3Iq.C/M0DqSYeRLijIDt9nLBX75COvlSH/6', 5),
-     (21, 'Cook', '$2b$10$p8cRiA5uu3BmMU0sllgo9u4LlVFhLkqmzhAfL6uWaP6SSm94kudeK', 201),
-     (22, 'ssupervisor', '$2b$10$SL3/vMtGu1.jBksJ4CkHt.SXl0Swi8Dc3km9a3i23gAXz8a5XG1Eq', 204),
-     (23, 'Supervisor', '$2b$10$FMQH8Hc/7HD5cfN2DT1vMuhA87j4o67Y1qwvbXOKm8BKJOiLzS7Bq', 204);
+	 ('Kitchen','cooking'),
+	 ('Cleaning','sweep and mop'),
+	 ('Front Staff','handle bookings and query resolution'),
+	 ('Management','handles customer and resolve complaints'),
+	 ('Commute','pick up and drop');
 
-INSERT INTO Stores (store_id, name, location, category, availability, rating)
-         VALUES
-          (1, 'Store1', 'Area1', 'Restaurant', 'Mon-Fri 10am-8pm', 2.93333),
-          (2, 'Store2', 'Area51', 'Cycle Repair', 'Mon-Fri 10am-2pm', NULL),
-          (3, 'Nescafe', NULL, 'Cafe', 'Sun-Sat 10AM-8PM', NULL),
-          (4, 'Cycle store', NULL, 'cycle repair', 'Mon-Fri 8AM-10AM', NULL),
-          (5, 'Saras General stores', 'Canteen', 'Stationery', 'Mon-Sat 10AM-7PM', NULL),
-          (6, 'Varchas Fruite', 'Sports Area', 'Vegetables and Fruits', 'Sat-Sun 8AM-10AM', NULL),
-          (7, 'Kitsons', 'Admin block', 'Cafe', 'Mon-Fri 8AM-12PM', NULL);
+INSERT INTO room_type(room_type_name, room_cost)
+    VALUES 
+	 ('Standard Room','110'),
+	 ('Standard Twin Room','120'),
+	 ('Executive Room','140'),
+	 ('Club Room','160'); 
+	 
+INSERT INTO guestHouse_services( service_name, service_description, service_cost, guestHouse_guestHouse_id)
+    VALUES
+	 ('24-hour Room Service','There will be 24-hour Room Service to take care of customers needs',20,1),
+	 ('Currency Exchange','Foreign Currency Exchange facility available',80,1),
+	 ('Laundry','Laundry/Dry Cleaning available same day',10,1),
+	 ('Entertainment Room','Book and watch movies',50,2),
+	 ('Sports Complex','Sports Complex access to all guests.',100,2),
+	 ('Gym','Access to Gym.',140,2);
 
-INSERT INTO Shopkeepers (SID, name, username, store_id, phone_no, sec_pass_id, sec_pass_exp)
-          VALUES
-              (1, 'anand', 'admin2', 1, 2312331245, 'S1234', '2022-02-04'),
-              (3, 'new', NULL, NULL, NULL, NULL, NULL),
-              (4, 'Pratik Kulparti', 'Jack', 2, 2429911234, 'S324', '2022-01-20'),
-              (5, 'mandeep ishar', 'CC1', 5, 8909080998, 'S23942', '2021-11-21'),
-              (6, 'sheru don', 'Cook', 4, 8009009008, 'S349', '2021-12-02'),
-              (7, 'Krishna Tulasi', 'Staff', 3, 7890988907, 'S3485', '2021-11-18');
+INSERT INTO employees(emp_first_name, emp_last_name, emp_designation, emp_contact_number, emp_email_address, department_department_id, addresses_address_id, guestHouse_guestHouse_id)
+    VALUES 
+     ('Rushi','Langde','Waiter','4545444443','rushi@gmail.com',1,11,1),
+     ('Ashish','Murthy','Manager','6565646463','ashish@gmail.com',4,12,1),
+     ('Parth','Gupta','Cashier','7575747473','parth@gmail.com',3,13,1),
+     ('Nikhil','Chobey','Cook','8585848483','nikhil@gmail.com',1,14,1),
+     ('Khanjan','Pathak','Manager','9595949493','khanjan@gmail.com',4,15,2);
+     
+	
+-- guesthouse have address id starting from 100
+INSERT INTO guestHouse( guestHouse_name, guestHouse_contact_number, guestHouse_description, guestHouse_floor_count, guestHouse_room_capacity, addresses_address_id, check_in_time, check_out_time)
+    VALUES 
+	 ('A Quarters','9595858575','A 5 min walk from gate 1', 10, 45, 101, '12:00:00','23:00:00'),
+	 ('B Quarters','9494848474','A 15 min walk from gate 1', 10, 55, 102, '12:00:00','23:00:00'),
+	 ('C Quarters','9393838373','A 5 min walk from gate 2', 10, 50, 103, '12:00:00','23:00:00'),
+	 ('D Quarters','9292828272','A 15 min walk from gate 2', 10, 40, 104, '12:00:00','23:00:00');
 
-INSERT INTO License (lic_id, expiry, store_id)
-          VALUES
-                   ('L0123', '2025-03-04', 1),
-                   ('L234', '2029-03-21', 3),
-                   ('L292', '2022-04-03', 2),
-                   ('L345', '2023-09-20', 4);
+-- preinserted guest details
+INSERT INTO guests(guest_first_name, guest_last_name, guest_contact_number, guest_email_address, guest_credit_card, guest_id_proof, addresses_address_id)
+    VALUES 
+	 ('Gaurav','Garg','8979695949','gaurav@gmail.com',NULL,'Driving License',1),
+	 ('Saurav','Dudhate','8878685848','saurav@gmail.com',NULL,'Passport',2),
+	 ('Shreyansh','Kumar','8777675747','sheru@gmail.com',NULL,'Aadhaar Card',3),
+	 ('Anurag','Deshmane','8676665646','anurag@gmail.com',NULL,'Aadhaar Card',4),
+	 ('Tingu','Aggarwal','8575655545','tingu@gmail.com',NULL,'Driving Lincense',5);
 
-INSERT INTO Feedback (feed_id, store_id, service, availability, quality, price, conduct, message)
-          VALUES
-                        (1, 1, 4, 5, 3, 4, 5, 'Quality of goods can be better.'),
-                        (2, 1, 1, 2, 3, 4, 5, NULL),
-                        (3, 1, 3, 3, 3, 2, 3, NULL),
-                        (4, 1, 1, 1, 1, 1, 1, NULL),
-                        (5, 1, 4, 4, 4, 5, 3, NULL),
-                        (6, 1, 2, 2, 2, 2, 5, NULL);
+-- booking insetion will be done next
+INSERT INTO bookings (booking_id, booking_date, duration_of_stay, check_in_date, check_out_date, booking_payment_type, total_rooms_booked, guestHouse_guestHouse_id, guests_guest_id, employees_emp_id, total_amount)
+    VALUES
+	 (1, '2022-08-08 00:00:00', '5', '2022-08-10 12:00:00', '2022-08-15 23:00:00', 'cash', 1, 101, 1, 2, '590'),
+	 (2, '2012206-08 00:00:00', '20', '2022-06-08 12:00:00', '2022-06-28 23:00:00', 'card', 1, 101, 2, 2, '2300'),
+	 (3, '2012206-08 00:00:00', '10', '2022-06-08 12:00:00', '2022-06-18 23:00:00', 'card', 1, 102, 1, 3, '1100'),
+	 (4, '2022-06-08 00:00:00', '2', '2022-06-08 12:00:00', '2022-06-10 23:00:00', 'card', 1, 102, 4, 2, '290'),
+	 (5, '2022-06-08 00:00:00', '3', '2022-06-08 12:00:00', '2022-06-11 23:00:00', 'card', 1, 102, 2, 3, '350'),
+	 (6, '2022-06-08 00:00:00', '5', '2022-06-08 12:00:00', '2022-06-13 23:00:00', 'card', 1, 103, 3, 3, '570'),
+	 (7, '2022-08-13 00:00:00', '2', '2022-06-13 12:00:00', '2022-06-15 23:00:00', 'cash', 2, 103, 5, 2, '280'),
+	 (8, '2022-08-10 00:00:00', '3', '2022-08-11 12:00:00', '2022-08-13 23:00:00', 'card', 1, 103, 3, 2, '350'),
+	 (9, '2022-08-10 00:00:00', '5', '2022-08-12 12:00:00', '2022-08-16 23:00:00', 'card', 1, 104, 4, 2, '570'),
+	 (10, '2022-08-14 00:00:00', '2', '2022-08-15 12:00:00', '2022-08-17 23:00:00', 'cash', 2, 104, 5, 2, '280'),
+	 (11, '2022-08-14 00:00:00', '5', '2022-08-16 12:00:00', '2022-08-21 23:00:00', 'cash', 1, 104, 1, 3, '590');
 
-INSERT INTO Billrequests (breq_id, paid_amt, tran_id, mode, status, pb_id)
-          VALUES
-                             (1, 1200, 'T1239', 'Card', 'Accepted', 1),
-                             (2, 2400, 'T192391', 'Card', 'In review', 2),
-                             (3, 2500, 'T342', 'Cash', 'In review', 4);
 
-INSERT INTO Pendingbills (pb_id, store_id, type_of_service, month_no, due_amt)
-          VALUES
-              (1, 1, 'Rent', '2020-12-01', 300),
-              (2, 2, 'Electricity', '2020-08-09', 4200),
-              (3, 2, 'Rent', '2021-12-12', 2000),
-              (4, 1, 'Rent', '2020-11-01', 3200);
+INSERT INTO rooms_mapping(room_id, room_number, rooms_type_rooms_type_id, guestHouse_guestHouse_id)
+    VALUES 
+	 (1,1001,1,101),
+	 (2,1002,1,101),
+	 (3,1003,1,101),
+	 (4,1004,1,101),
+	 (5,1005,1,101),
+	 (6,1006,3,101),
+	 (7,1007,3,101),
+	 (8,1008,3,101),
+	 (9,1009,3,101),
+	 (10,1010,3,101),
+	 (11,2001,1,102),
+	 (12,2002,1,102),
+	 (13,2003,1,102),
+	 (14,2004,1,102),
+	 (15,2005,1,102),
+	 (16,2006,2,102),
+	 (17,2007,2,102),
+	 (18,2008,2,102),
+	 (19,2009,2,102),
+	 (20,2010,2,102),
+	 (21,3001,2,103),
+	 (22,3002,2,103),
+	 (23,3003,2,103),
+	 (24,3004,2,103),
+	 (25,3005,2,103),
+	 (26,3006,4,103),
+	 (27,3007,4,103),
+	 (28,3008,4,103),
+	 (29,3009,4,103),
+	 (30,3010,4,103),
+	 (31,4001,2,104),
+	 (32,4002,2,104),
+	 (33,4003,2,104),
+	 (34,4004,2,104),
+	 (35,4005,2,104),
+	 (36,4006,2,104),
+	 (37,4007,2,104),
+	 (38,4008,2,104),
+	 (39,4009,2,104),
+	 (40,4010,2,104);
 
-INSERT INTO License_ext_req (er_id, lic_id, extension_period_days, fee_paid, status, tran_id, mode)
-          VALUES
-              (2, 'L0123', 3, 2000, 'On hold', 'T1293', 'Card'),
-              (3, 'L345', 4, 3000, 'In review', 'T3453', 'Cash'),
-              (4, 'L292', 3, 2000, 'Denied', 'T239', 'Cheque');
 
-          
+INSERT INTO guestHouse_services_used_by_guests (guestHouse_services_service_id, bookings_booking_id) 
+    VALUES 
+     (1, 2),
+     (2, 2),
+     (3, 2),
+     (4, 4),
+     (6, 6),
+     (1, 7),
+     (4, 3),
+     (5, 1),
+     (3, 8),
+     (1, 10);
+
+INSERT INTO rooms_booked (bookings_booking_id, rooms_room_id) 
+    VALUES 
+    (1,1),
+    (2,2),
+    (3,11),
+    (4,12),
+    (5,13),
+    (6,21),
+    (7,22),
+    (7,23),
+    (8,24),
+    (9,31),
+    (10,32),
+    (11,33);
+    
--- tables.sql
+++ tables.sql
@@ -1,92 +1,225 @@
-create table Users (
-    UID int not null primary key ,
-    username varchar(30),
-    password varchar(100),
-    role_id int
+CREATE TABLE addresses (
+  address_id INT NOT NULL,
+  address_line1 VARCHAR(100) NULL,
+  address_line2 VARCHAR(100) NULL,
+  city VARCHAR(45) NULL,
+  state VARCHAR(45) NULL,
+  country VARCHAR(45) NULL,
+  zipcode VARCHAR(8) NULL,
+  PRIMARY KEY (address_id)
 );
 
-create table Stores(
-    store_id int not null primary key ,
-    name varchar(30),
-    location varchar(30),
-    category varchar(30),
-    availability text,
-    rating float
+CREATE TABLE guestHouse (
+  guestHouse_id INT NOT NULL AUTO_INCREMENT,
+  guestHouse_name VARCHAR(45) NULL,
+  guestHouse_contact_number VARCHAR(12) NULL,
+  guestHouse_description VARCHAR(100) NULL,
+  guestHouse_floor_count INT NULL,
+  guestHouse_room_capacity INT NULL,
+  addresses_address_id INT NOT NULL ,
+  check_in_time TIME NULL,
+  check_out_time TIME NULL,
+  PRIMARY KEY (guestHouse_id, addresses_address_id),
+  CONSTRAINT fk_guestHouses_addresses1
+    FOREIGN KEY (addresses_address_id)
+    REFERENCES addresses (address_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = '		';
+
+CREATE INDEX fk_guestHouses_addresses1_idx ON guestHouse (addresses_address_id ASC);
+
+
+CREATE TABLE room_type (
+  room_type_id INT NOT NULL AUTO_INCREMENT,
+  room_type_name VARCHAR(45) NULL,
+  room_cost DECIMAL(10,2) NULL,
+  PRIMARY KEY (room_type_id)
 );
 
-create table Shopkeepers(
-    SID int primary key not null AUTO_INCREMENT,
-    name varchar(30),
-    sec_pass_exp date,
-    phone_no bigint,
-    sec_pass_id varchar(20),
-    store_id int not null,
-    username varchar(30),
-    CONSTRAINT fk_Shopkeepers_Stores1
-    foreign key (store_id) references Stores(store_id),
-    CONSTRAINT fk_Shopkeepers_Users1
-    foreign key (username) references Users(username)
+CREATE TABLE rooms_mapping (
+  room_id INT NOT NULL,
+  room_number INT(4) NULL,
+  rooms_type_rooms_type_id INT NOT NULL,
+  guestHouse_guestHouse_id INT NOT NULL,
+  available int default 1,
+  PRIMARY KEY (room_id, rooms_type_rooms_type_id, guestHouse_guestHouse_id),
+  CONSTRAINT fk_rooms_rooms_type1
+    FOREIGN KEY (rooms_type_rooms_type_id)
+    REFERENCES room_type (room_type_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_rooms_guestHouse1
+    FOREIGN KEY (guestHouse_guestHouse_id)
+    REFERENCES guestHouse (guestHouse_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-create index fk_Shopkeepers_Stores1_idx on Shopkeepers(store_id ASC);
-create index fk_Shopkeepers_Users1_idx on Shopkeepers(username ASC);
+CREATE INDEX fk_rooms_rooms_type1_idx ON rooms_mapping (rooms_type_rooms_type_id ASC);
+CREATE INDEX fk_rooms_guestHouse1_idx ON rooms_mapping (guestHouse_guestHouse_id ASC);
 
-create table License(
-    lic_id VARCHAR(30) not null primary key,
-    expiry date,
-    store_id int not null,
-    CONSTRAINT fk_License_Stores1
-    foreign key (store_id) references Stores(store_id)
+CREATE TABLE guests(
+  guest_id INT NOT NULL AUTO_INCREMENT,
+  guest_first_name VARCHAR(45) NULL,
+  guest_last_name VARCHAR(45) NULL,
+  guest_contact_number VARCHAR(12) NULL,
+  guest_email_address VARCHAR(45) NULL,
+  guest_credit_card VARCHAR(45) NULL,
+  guest_id_proof VARCHAR(45) NULL,
+  addresses_address_id INT NOT NULL,
+  PRIMARY KEY (guest_id, addresses_address_id),
+  CONSTRAINT fk_guests_addresses1
+    FOREIGN KEY (addresses_address_id)
+    REFERENCES addresses (address_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = '	';
+
+CREATE INDEX fk_guests_addresses1_idx ON guests (addresses_address_id ASC);
+
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(45) NULL,
+  department_description VARCHAR(100) NULL,
+  PRIMARY KEY (department_id)
 );
 
-create index fk_License_Stores1_idx on License(store_id ASC);
-
-create table Feedback(
-    feed_id int not null primary key,
-    store_id int not null,
-    service int not null,
-    availability int not null,
-    quality int not null,
-    price int not null,
-    conduct int not null,
-    message varchar(100),
-    CONSTRAINT fk_Feedback_Stores1
-    foreign key (store_id) references Stores(store_id)
+CREATE TABLE employees (
+  emp_id INT NOT NULL AUTO_INCREMENT,
+  emp_first_name VARCHAR(45) NULL,
+  emp_last_name VARCHAR(45) NULL,
+  emp_designation VARCHAR(45) NULL,
+  emp_contact_number VARCHAR(12) NULL,
+  emp_email_address VARCHAR(45) NULL,
+  department_department_id INT NOT NULL,
+  addresses_address_id INT NOT NULL,
+  guestHouse_guestHouse_id INT NOT NULL,
+  PRIMARY KEY (emp_id, department_department_id, addresses_address_id, guestHouse_guestHouse_id),
+  CONSTRAINT fk_employees_services1
+    FOREIGN KEY (department_department_id)
+    REFERENCES department (department_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_employees_addresses1
+    FOREIGN KEY (addresses_address_id)
+    REFERENCES addresses (address_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_employees_guestHouse1
+    FOREIGN KEY (guestHouse_guestHouse_id)
+    REFERENCES guestHouse (guestHouse_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-create index fk_Feedback_Stores1_idx on Feedback(store_id ASC);
+CREATE INDEX fk_employees_services1_idx ON employees (department_department_id ASC);
+CREATE INDEX fk_employees_addresses1_idx ON employees (addresses_address_id ASC);
+CREATE INDEX fk_employees_guestHouse1_idx ON employees (guestHouse_guestHouse_id ASC);
 
-create table Billrequests(
-    breq_id int not null primary key,
-    paid_amt int,
-    tran_id varchar(30),
-    mode varchar(30),
-    status varchar(30),
-    pb_id int
+CREATE TABLE bookings (
+  booking_id INT NOT NULL,
+  booking_date DATETIME NULL,
+  duration_of_stay VARCHAR(10) NULL,
+  check_in_date DATETIME NULL,
+  check_out_date DATETIME NULL,
+  booking_payment_type VARCHAR(45) NULL,
+  total_rooms_booked INT NULL,
+  guestHouse_guestHouse_id INT NOT NULL,
+  guests_guest_id INT NOT NULL,
+  employees_emp_id INT NOT NULL,
+  total_amount DECIMAL(10,2) NULL,
+  PRIMARY KEY (booking_id, guestHouse_guestHouse_id, guests_guest_id, employees_emp_id),
+  CONSTRAINT fk_bookings_guestHouse1
+    FOREIGN KEY (guestHouse_guestHouse_id)
+    REFERENCES guestHouse (guestHouse_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_bookings_guests1
+    FOREIGN KEY (guests_guest_id)
+    REFERENCES guests (guest_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_bookings_employees1
+    FOREIGN KEY (employees_emp_id)
+    REFERENCES employees (emp_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-create table Pendingbills(
-    pb_id int not null primary key,
-    type_of_service varchar(30),
-    due_amt int,
-    month_no date,
-    store_id int not null,
-    CONSTRAINT fk_Pendingbills_Stores1
-    foreign key (store_id) references Stores(store_id)
+CREATE INDEX fk_bookings_guestHouse1_idx ON bookings (guestHouse_guestHouse_id ASC);
+CREATE INDEX fk_bookings_guests1_idx ON bookings (guests_guest_id ASC);
+CREATE INDEX fk_bookings_employees1_idx ON bookings (employees_emp_id ASC);
+
+CREATE TABLE rooms_booked(
+  rooms_booked_id INT NOT NULL AUTO_INCREMENT,
+  bookings_booking_id INT NOT NULL,
+  rooms_room_id INT NOT NULL,
+  PRIMARY KEY (rooms_booked_id, bookings_booking_id, rooms_room_id),
+  CONSTRAINT fk_rooms_booked_bookings1
+    FOREIGN KEY (bookings_booking_id)
+    REFERENCES bookings (booking_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_rooms_booked_rooms1
+    FOREIGN KEY (rooms_room_id)
+    REFERENCES rooms_mapping(room_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-create index fk_Pendingbills_Stores1_idx on Pendingbills(store_id);
+CREATE INDEX fk_rooms_booked_bookings1_idx ON rooms_booked (bookings_booking_id ASC);
+CREATE INDEX fk_rooms_booked_rooms1_idx ON rooms_booked (rooms_room_id ASC);
 
-create table License_ext_req(
-    er_id int not null primary key,
-    lic_id varchar(30) not null,
-    extension_period_days int,
-    fee_paid int,
-    status varchar(20),
-    tran_id varchar(30),
-    mode varchar(10),
-    CONSTRAINT fk_License_ext_req_License1
-    foreign key (lic_id) references License(lic_id)
+CREATE TABLE guestHouse_services (
+  service_id INT NOT NULL AUTO_INCREMENT,
+  service_name VARCHAR(45) NULL,
+  service_description VARCHAR(100) NULL,
+  service_cost DECIMAL(10,2) NULL,
+  guestHouse_guestHouse_id INT NOT NULL,
+  PRIMARY KEY (service_id, guestHouse_guestHouse_id),
+  CONSTRAINT fk_guestHouse_services_guestHouse1
+    FOREIGN KEY (guestHouse_guestHouse_id)
+    REFERENCES guestHouse (guestHouse_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-create index fk_License_ext_req_License1_idx on License_ext_req(lic_id ASC);
+CREATE INDEX fk_guestHouse_services_guestHouse1_idx ON guestHouse_services (guestHouse_guestHouse_id ASC);
+	
+CREATE TABLE guestHouse_services_used_by_guests (
+  service_used_id INT NOT NULL AUTO_INCREMENT,
+  guestHouse_services_service_id INT NOT NULL,
+  bookings_booking_id INT NOT NULL,
+  PRIMARY KEY (service_used_id, guestHouse_services_service_id, bookings_booking_id),
+  CONSTRAINT fk_guestHouse_services_has_bookings_guestHouse_services1
+    FOREIGN KEY (guestHouse_services_service_id)
+    REFERENCES guestHouse_services (service_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_guestHouse_services_has_bookings_bookings1
+    FOREIGN KEY (bookings_booking_id)
+    REFERENCES bookings (booking_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE INDEX fk_guestHouse_services_has_bookings_bookings1_idx ON guestHouse_services_used_by_guests (bookings_booking_id ASC);
+CREATE INDEX fk_guestHouse_services_has_bookings_guestHouse_services1_idx ON guestHouse_services_used_by_guests (guestHouse_services_service_id ASC);
+
+CREATE TABLE Bookings_Audit(
+ audit_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ booking_id INT NOT NULL,
+ booking_date DATETIME NULL,
+ duration_of_stay VARCHAR(10) NULL,
+ check_in_date DATETIME NULL,
+ check_out_date DATETIME NULL,
+ booking_payment_type VARCHAR(45) NULL,
+ total_rooms_booked INT NULL,
+ guestHouse_guestHouse_id INT NOT NULL,
+ guests_guest_id INT NOT NULL,
+ employees_emp_id INT NOT NULL,
+ total_amount DECIMAL(10,2) NULL,
+ action_type VARCHAR(50) NOT NULL,
+ date_updated DATETIME NOT NULL
+);
